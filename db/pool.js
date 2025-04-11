aa3ea5950d28d6c28b471f54195df5167e651178
adams <adamskiptalam0@gmail.com>
2025-04-11 22:24:23 +0300

    Add configuration to connct external postgres

--- db/pool.js
+++ db/pool.js
@@ -3,4 +3,7 @@ const { Pool } = require("pg");
 
 module.exports = new Pool({
 	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false, // Required for Render or other hosted PostgreSQL
+	},
 });
